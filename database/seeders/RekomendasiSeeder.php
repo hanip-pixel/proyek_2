bade454f159bc60d9dbe9dd6171cbef9091da82c
Hanif Mustofa <hanifindramayou@gmail.com>
2025-10-06 08:20:35 +0700

    database

--- database/seeders/RekomendasiSeeder.php
+++ database/seeders/RekomendasiSeeder.php
@@ -19,7 +19,28 @@ class RekomendasiSeeder extends Seeder
             'stok' => 17,
             'terjual' => 73,
             'foto_produk' => 'sweety.jpg',
-            'deskripsi' => '- Merek: Sweety - Bentuk: Popok celana sekali pakai - Warna: Putih dengan motif warna-warni - Komposisi utama: Bahan lembut dan elastis, lapisan penyerap, pelindung anti bocor - Manfaat: Daya serap tinggi untuk menjaga kulit bayi tetap kering, Bahan lembut dan nyaman untuk bayi aktif, Desain elastis yang pas di tubuh bayi tanpa meninggalkan bekas merah',
+            'deskripsi' => '- Merek: Sweety- Bentuk: Popok celana sekali pakai- Warna: Putih dengan motif warna-warni- Komposisi utama: Bahan lembut dan elastis, lapisan penyerap, pelindung anti bocor- Manfaat: Daya serap tinggi untuk menjaga kulit bayi tetap kering, Bahan lembut dan nyaman untuk bayi aktif, Desain elastis yang pas di tubuh bayi tanpa meninggalkan bekas merah',
+
+        ]);
+        Rekomendasi::create([
+            'nama_produk' => 'Susu Bear Brand',
+            'merek' => 'Nestle',
+            'harga_produk' => 10000,
+            'stok' => 55,
+            'terjual' => 161,
+            'foto_produk' => 'bear_brand.jpg',
+            'deskripsi' => '- Isi: 189ml per kaleng- Bentuk: Cair, susu steril siap minum- Warna: Putih susu- Komposisi utama: 100% susu sapi murni yang telah disterilisasi- Manfaat: Membantu menjaga daya tahan tubuh, Sumber nutrisi untuk mendukung kesehatan tubuh',
+            
+        ]);
+        Rekomendasi::create([
+            'nama_produk' => 'Gas Elpiji 3kg',
+            'merek' => 'Pertamina',
+            'harga_produk' => 23000,
+            'stok' => 47,
+            'terjual' => 41,
+            'foto_produk' => 'gas.jpg',
+            'deskripsi' => '- Merek: Pertamina- Isi: 3 kg gas LPG (Liquefied Petroleum Gas)- Bentuk: Tabung bulat berwarna hijau dengan katup pengaman- Komposisi utama: Campuran propana dan butana- Manfaat: Digunakan sebagai bahan bakar untuk memasak di rumah tangga, Hemat dan efisien untuk kebutuhan dapur sehari hari, Mudah digunakan dengan regulator khusus- Cara penggunaan: Pasangkan dengan regulator gas dan selang, gunakan sesuai petunjuk keamanan',
+            
         ]);
     }
 }
